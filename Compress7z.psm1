0277c9cfa2358636a5038755afbcbcc4c842aad0
coumbsek <coumbs.ek@gmail.com>
2018-08-25 22:24:16 +0200

    Add base for future Compression interface for 7zip

---
+++ Compress7z.psm1
@@ -0,0 +1,18 @@
+﻿Function Ini-CompressModule(){
+    $SevenZipInstallPath = "E:\ProgramFiles\7-Zip\7z.exe"  
+    set-alias SevenZip  $SevenZipInstallPath 
+    # use remove-item alias:SevenZip  if you want to remove t later
+}
+
+Function Compress-ArchiveForTransfer($Path, $DestinationPath, [switch]$Force, [switch]$Update){7
+    if ($Update){
+        SevenZip u $destinationPath $path
+    }
+    if ($Force){
+        SevenZip a
+    }
+}
+
+Function Expand-ArchiveForTransfer($Path, $DestinationPath, [switch]$Force, [switch]$Update){
+    SevenZip e $destinationPath $path
+}
