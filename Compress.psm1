f99d17d2c5e57ee0da972d4d1f340e4bd5bf41d1
coumbsek <coumbs.ek@gmail.com>
2018-08-25 22:23:48 +0200

    Add Compression abstraction for basic powershell CmdLet

---
+++ Compress.psm1
@@ -0,0 +1,20 @@
+﻿Function Ini-CompressModule(){
+}
+
+Function Compress-ArchiveForTransfer($Path, $DestinationPath, [switch]$Force, [switch]$Update){
+    if ($Force){
+        Compress-Archive -Path:$Path -DestinationPath:$DestinationPath -CompressionLevel:NoCompression -Force:$Force
+    }elseif($Update){
+        Compress-Archive -Path:$Path -DestinationPath:$DestinationPath -CompressionLevel:NoCompression -Update:$Update
+    }else{
+        Compress-Archive -Path:$Path -DestinationPath:$DestinationPath -CompressionLevel:NoCompression
+    }
+}
+
+Function Expand-ArchiveForTransfer($Path, $DestinationPath, [switch]$Force){
+    if ($Force){
+        Expand-Archive -Path:$Path -DestinationPath:$DestinationPath -Force:$Force
+    }else{
+        Expand-Archive -Path:$Path -DestinationPath:$DestinationPath
+    }
+}
