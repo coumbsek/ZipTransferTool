70435586c9aa06fe77b3e55b62c33e22ab6b324e
coumbsek <coumbs.ek@gmail.com>
2018-08-26 09:54:35 +0200

    Pre-check source path existence before copy

--- Copy.psm1
+++ Copy.psm1
@@ -1,3 +1,21 @@
-﻿Function Copy-ItemForTransfer($Path, $DestinationPath, [switch]$Force){
-    Copy-Item -Path:$Path -Destination:$DestinationPath -Force:$Force
+﻿Import-Module ('{0}\Scripts' -f $modulesPaths)
+
+Function Copy-ItemForTransfer($Path, $DestinationPath, [switch]$Force){
+    if (-not (Test-Path $Path)){
+        Write-Error ("Path {0} not existing" -f $Path)
+        return
+    }
+    $canContinue = $True
+    $folder = Get-Item $Path
+    $fullDestinationPath = ("{0}\{1}" -f $DestinationPath,$folder.Name)
+    if (Test-Path -PathType Container $fullDestinationPath){
+        $eraseAnyWay = -1
+        $eraseAnyWay = Select-Item -Caption:"Already Existing Folder" -Message:"Do you want to: " -choice:"&Cancel","&OverWrite"
+        if ($eraseAnyWay -eq 0){$canContinue=$False}
+    }
+    if ($canContinue -eq $True){
+        Copy-Item -Path:$Path -Destination:$DestinationPath -Force:$Force -Recurse
+    }
 }
+
+Export-ModuleMember -Function "Copy-ItemForTransfer"
--- CopyRobocopy.psm1
+++ CopyRobocopy.psm1
@@ -1,6 +1,22 @@
-﻿Function Copy-ItemForTransfer($Path, $DestinationPath, [switch]$Force){
-    Copy-Item -Path:$Path -Destination:$DestinationPath -Force:$Force
+﻿Import-Module ('{0}\Scripts' -f $modulesPaths)
+
+Function Copy-ItemForTransfer($Path, $DestinationPath, [switch]$Force){
+    if (-not (Test-Path $Path)){
+        Write-Error ("Path {0} not existing" -f $Path)
+        return
+    }
     $folder = Get-Item $Path
-    $fullDestinationPath = ("{0}\{1}" -f $DestinationPath,)
-    Test-Path
+    $fullDestinationPath = ("{0}\{1}" -f $DestinationPath,$folder.Name)
+    $canContinue = $True
+    
+    if (Test-Path -PathType Container $fullDestinationPath){
+        $eraseAnyWay = Select-Item -Caption:"Already Existing Folder" -Message:"Do you want to: " -choice:"&Cancel","&OverWrite"
+        if ($eraseAnyWay -eq 0){$canContinue=$False}
+    }
+    if ($canContinue -eq $True){
+        mkdir $fullDestinationPath
+        Robocopy $Path $fullDestinationPath /e
+    }
 }
+
+Export-ModuleMember -Function "Copy-ItemForTransfer"
