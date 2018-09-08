30650999f8e63241199882332e7a084f3a9ed330
coumbsek <coumbs.ek@gmail.com>
2018-09-08 11:39:50 +0200

    Addind Code signing utility

---
+++ CodeSigning.psm1
@@ -0,0 +1,21 @@
+﻿$binSDKPath = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x86"
+Set-Alias makecert ("{0}\makecert.exe" -f $binSDKPath)
+Set-Alias signtool ("{0}\signtool.exe" -f $binSDKPath)
+
+
+$XCN_OID_PKIX_KP_CODE_SIGNING = "1.3.6.1.5.5.7.3.3"
+
+Function New-CertificateAuthority($NameOfCA){
+    $fullNameOfCA = ("CN={0} Local Authority" -f $NameOfCA)
+    makecert -n $fullNameOfCA -a sha1 -eku $XCN_OID_PKIX_KP_CODE_SIGNING -r -sv LDPowerShell.pvk LDPowerShell.cer -ss Root -sr localMachine
+    makecert -pe -n $fullNameOfCA -ss My -a sha1 -eku $XCN_OID_PKIX_KP_CODE_SIGNING -iv LDPowerShell.pvk -ic LDPowerShell.cer
+}
+
+Function Set-Certificate($NameOfCA, $path){
+    $cert= Get-ChildItem Cert:\CurrentUser\My | where { $_ -match $NameOfCA}
+    Set-AuthenticodeSignature $path $cert
+}
+
+Function Assert-PowershellAuthenticity(){
+    signtool verify /c KB926140.CAT powershell.exe
+}
